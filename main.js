8719b4f6d62d2df2c9d4c6841863fb18082b5507
Carlos Campbell <carlos.g.campbell@gmail.com>
2024-03-06 20:08:32 -0500

    notes started

---
+++ main.js
@@ -0,0 +1,40 @@
+//Modules are different from "Module pattern" that was taught in the class 2.
+
+//Modules deal with npm and webpack which are actually simple to implement 
+
+
+/******************************************************************************** **/
+
+//a link about npm 
+// https://docs.npmjs.com/about-npm
+
+//npm is the worlds largest software registry, it consists of 3 distinct components
+
+//the website, the command line interface (CLI), and the registry
+
+//the website is used to discover packages, set up profiles, and manage other aspects of your npm experience.
+
+//the CLI runs from a terminal and that is how developers interact with npm
+
+//the registry is a large public database of Javascript and meta-information
+
+
+
+//so what is npm used for?
+
+//a few examples are to adapt packages of code for apps, download standalone tools, run packages without downloading, sharing code with npm users
+
+//to use it fully you need to create an account and install npm (i believe because i have node that this is already done for me)
+
+/****************************************************************************** **/
+
+
+//There is something that exists called Yarn, it is a replacement for the default npm. It has it's own uses and features and recent versions of npm use some of yarns best features. Just something to be aware of that exists.
+
+
+
+//rather than having one really large file with lots of code, it's better to have a bunch of multiple smaller files (modules)
+
+//a bundler 
+
+//you can only default export one thing
